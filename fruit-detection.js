ca79f753026ed0145acb35169a1fa9cada498c48
Ashley Matthews <ashley.matthews6@gmail.com>
2025-04-26 14:39:18 -0400

    Create fruit-detection.js

---
+++ fruit-detection.js
@@ -0,0 +1,13 @@
+app.post('/put-your-unique-path-here', (req, res) => {
+  try {
+    const fruitToCheck = req.body.fruit;
+    if (fruits.includes(fruitToCheck)) {
+      res.send(200, "Fruit is known");
+    } else {
+      res.send(400, "Fruit is not known");
+    }
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
